16d98cb339d2b41a01e7b14a0b0ebf54e20f5b1f
Dragzss69 <lionelmansa2596@gmail.com>
2026-04-16 01:02:52 +0800

    chapter 1 nya  ditambah

--- app/src/main/java/com/suryakusuma/novelin/NovelData.java
+++ app/src/main/java/com/suryakusuma/novelin/NovelData.java
@@ -12,63 +12,63 @@ public class NovelData {
                 "Wilkie Collinsq",
                 "A ghostly woman warns a beautiful Victorian heiress about a count, and a strange spell haunts a mansion...",
                 R.drawable.novel12,
-                Arrays.asList(new Novel.Chapter("Chapter 1: Once upon a time...", "woman_in_white.txt"))
+                Arrays.asList(new Novel.Chapter("Chapter 1: Walter Hartright, a drawing master who secures a position at Limmeridge House through the help of his friend, Professor Pesca. However, on the night before his departure, the atmosphere turns chilling when he encounters a mysterious woman dressed entirely in white on a lonely road in Hampstead. The woman appears terrified, asks for directions to London, and mentions her deep affection for the Fairlie family in Cumberland before Walter eventually helps her escape. Shortly after she disappears, Walter is startled by a group of men searching for her, who reveal the shocking fact that she has just escaped from a lunatic asylum, leaving Walter with a sense of dread and a lingering feeling that he has just stepped into a dark mystery.", "woman_in_white.txt"))
         ));
         list.add(new Novel(
                 "And Then There Were None",
                 "Agatha Christie",
                 "Europe teeters on the brink of war. Ten strangers are invited to Soldier Island, an isolated rock near the Devon coast...",
                 R.drawable.novel11,
-                Arrays.asList(new Novel.Chapter("Chapter 1: Once upon a time...", "and_then_there_were_none.txt"))
+                Arrays.asList(new Novel.Chapter("Chapter 1: Justice Wargrave, Vera Claythorne, Philip Lombard, Emily Brent, General Macarthur, Dr. Armstrong, Tony Marston, and Mr. Blore—as they travel by train or car toward a mysterious destination called Soldier Island off the coast of Devon. Each character has been lured there under different pretexts, ranging from a summer job or a medical consultation to a simple invitation from an old friend, yet they all share a sense of curiosity about their elusive host, Mr. U.N. Owen. As they journey, the narrative subtly reveals glimpses of their diverse social backgrounds and hidden anxieties, building an atmosphere of suspense that culminates when they finally gather at the shoreline, oblivious to the fact that their shared presence on the isolated island is part of a dark and calculated scheme.", "and_then_there_were_none.txt"))
         ));
         list.add(new Novel(
                 "The Midnight Library",
                 "Matt Haig",
                 "a woman named Nora Seed, who after reaching a breaking point in her life, finds herself in a mystical library...",
                 R.drawable.novel10,
-                Arrays.asList(new Novel.Chapter("Chapter 1: Once upon a time...", "midnight_library.txt"))
+                Arrays.asList(new Novel.Chapter("Chapter 1: A woman overwhelmed by \"rootlessness\" and a crushing sense of failure, nineteen years before she decides to die. The narrative quickly shifts to her present-day reality in Bedford, where her life unravels in a series of devastating blows: she loses her job at a music shop, her only piano student cancels their lessons, and she learns that her beloved cat, Voltaire, has died in the street. These events serve as the final catalysts for her deep-seated despair, reinforcing her belief that she is surplus to requirements and has let everyone down. Spiraling into a dark \"black hole\" of regret, Nora writes a suicide note, concluding that her life is a mess she no longer wishes to manage, which sets the stage for her arrival at the mystical library between life and death.", "midnight_library.txt"))
         ));
         list.add(new Novel(
                 "Harry Potter",
                 "J.K. Rowling",
                 "Harry is described as having his father's perpetually untidy black hair, his mother's bright green eyes, and a lightning bolt-shaped scar on his forehead..",
                 R.drawable.novel9,
-                Arrays.asList(new Novel.Chapter("Chapter 1: Once upon a time...", "harry_potter.txt"))
+                Arrays.asList(new Novel.Chapter("Chapter 1: Begins with the mundane and judgmental lives of Mr. and Mrs. Dursley, who pride themselves on being perfectly normal and despise anything mysterious or eccentric. Their world is turned upside down as they notice strange occurrences throughout the day—cats reading maps, people in colorful cloaks whispering about a family called the Potters, and a flurry of owls flying overhead. That night, the powerful wizard Albus Dumbledore meets Professor McGonagall and Rubeus Hagrid on Privet Drive to deliver the orphaned Harry Potter to the Dursleys' doorstep. They reveal that Harry’s parents were killed by the dark wizard Lord Voldemort, but Harry miraculously survived the attack with only a lightning-bolt scar, becoming the boy who lived and the hidden savior of the wizarding world.", "harry_potter.txt"))
         ));
         list.add(new Novel(
                 "Metamorphosis",
                 "Franz Kafka",
                 "Gregor Samsa, from human to insect and the complications he faces in his new form...",
                 R.drawable.novel8,
-                Arrays.asList(new Novel.Chapter("Chapter 1: he woke up with the body of a cockroach...", "metamorphosis.txt"))
+                Arrays.asList(new Novel.Chapter("Chapter 1: Gregor Samsa waking up in his bed to discover that he has been transformed into a monstrous vermin. Despite his horrific physical state—a hard shell-like back, a segmented belly, and numerous thin, flickering legs—Gregor’s primary concern remains tragically mundane: his anxiety over being late for his job as a traveling salesman and his fear of his overbearing boss. As his family and the office chief clerk gather outside his locked bedroom door, demanding he come out, Gregor struggles to maneuver his new, clumsy body to open the door. When he finally succeeds, the sight of his grotesque form sends the chief clerk fleeing in terror and leaves his family in a state of shock and grief, ending with his father violently herding a wounded and confused Gregor back into his room, effectively sealing his isolation from the human world.", "metamorphosis.txt"))
         ));
         list.add(new Novel(
                 "The Little Prince",
                 "Antoine de Saint-Exupéry",
                 "The titular character who travels from planet to planet...",
                 R.drawable.novel7,
-                Arrays.asList(new Novel.Chapter("Chapter 1: He is a young, curious boy with golden hair who asks many questions...", "little_prince.txt"))
+                Arrays.asList(new Novel.Chapter("Chapter 1: A pilot who reflects on his childhood frustration with the grown-up world's lack of imagination. He recalls his Drawing Number One, which depicted a boa constrictor digesting an elephant, only for adults to dismissively mistake it for a simple hat. After a second attempt to clarify the image by drawing the inside of the snake was met with advice to abandon art and focus instead on geography, history, and arithmetic, the narrator gives up his dream of becoming a painter. This early disappointment leads him to a solitary life as a pilot, where he periodically tests the intelligence of adults he meets with his old drawing, only to find that they all see the same hat, confirming his belief that grown-ups are unable to perceive the true essence of things without tedious explanations.", "little_prince.txt"))
         ));
         list.add(new Novel(
                 "If Cat Disappeared from the World",
                 "Genki Kawamura",
                 "Young mailman finds out he has no time left due to a terminal disease...",
                 R.drawable.novel6,
-                Arrays.asList(new Novel.Chapter("Chapter 1: Suddenly he is approached by a devil...", "if_cat_disappeared.txt"))
+                Arrays.asList(new Novel.Chapter("Chapter 1: A thirty-year-old postman who leads a quiet, solitary life until he is suddenly diagnosed with a terminal brain tumor and told he has only days to live. Upon returning home in despair, he encounters a flamboyant, hawaiian-shirt-wearing doppelgänger of himself who claims to be the Devil (Aloha). The Devil offers a bizarre bargain: for every item the protagonist agrees to make disappear from the entire world, he will gain one extra day of life. The chapter explores the protagonist's internal struggle and existential dread as the Devil chooses phones as the first item to vanish, forcing the narrator to confront how much of his human connection and memory is tied to a device he is about to lose forever.", "if_cat_disappeared.txt"))
         ));
         list.add(new Novel(
                 "Animal Farm",
                 "George Orwell",
                 "a novel about a group of animals who take control of the farm they live on...",
                 R.drawable.novel4,
-                Arrays.asList(new Novel.Chapter("Chapter 1: on a farm...", "animal_farm.txt"))
+                Arrays.asList(new Novel.Chapter("Chapter 1: The prize Middle White boar, gathering all the animals of Manor Farm in the big barn to share a revolutionary dream. After the drunken farmer, Mr. Jones, goes to bed, Major delivers a stirring speech denouncing the tyranny of mankind and explaining that all animals are comrades, while all men are enemies. He introduces the idea that animals can live in a society free from human exploitation and teaches them the anthem Beasts of England, which sparks a surge of rebellious hope and unity among the creatures. Although Old Major dies shortly after, his vision sets the stage for the inevitable uprising, establishing the philosophical foundation for what will eventually become the Seven Commandments of Animalism.", "animal_farm.txt"))
         ));
         list.add(new Novel(
                 "Don Quixote",
                 "Miguel De Cervantes Saavedra",
                 "The middle-aged gentleman from La Mancha who becomes obsessed with chivalry books and decides to become a knight-errant...",
                 R.drawable.novel3,
-                Arrays.asList(new Novel.Chapter("Chapter 1: Once upon a time...", "don_quixote.txt"))
+                Arrays.asList(new Novel.Chapter("Chapter 1: a middle-aged hidalgo living in a village in La Mancha, who becomes so obsessed with reading books of chivalry that he loses his mind and decides to become a knight-errant himself. Neglecting his estate and forgoing sleep, he believes every fantastical tale of giants, enchantments, and battles to be historical fact, leading him to polish some old, rusty armor and fashion a makeshift helmet out of cardboard. He renames his weary, skinny horse Rocinante, adopts the more noble title of Don Quixote de la Mancha, and chooses a local farm girl to be his lady-love under the name Dulcinea del Toboso, all to fulfill his romantic vision of roaming the world to right wrongs and achieve eternal fame.", "don_quixote.txt"))
         ));
         list.add(new Novel(
                 "Solo Leveling",
@@ -76,8 +76,8 @@ public class NovelData {
                 "In a world where hunters, humans who possess magical abilities, must battle deadly monsters...",
                 R.drawable.novel13,
                 Arrays.asList(
-                    new Novel.Chapter("Chapter 1: The Weakest Hunter", "solo_leveling.txt"),
-                    new Novel.Chapter("Chapter 2: The Double Dungeon", "solo_leveling.txt")
+                    new Novel.Chapter("Chapter 1: Sung Jin-Woo, known as the World's Weakest Hunter, who risks his life in low-rank dungeons just to pay for his mother’s hospital bills. Despite his constant injuries and lack of talent, he joins a raid on a D-Rank dungeon with a group of hunters, but the mission takes a terrifying turn when they discover a hidden, mysterious double dungeon deep inside. As they enter the massive chamber filled with ominous stone statues, the door slams shut, and they realize they are trapped in a deadly trial governed by the Commandments of the Cartenon Temple. The chapter concludes with a sudden and brutal show of power from the statues, leaving the group in a state of absolute terror and setting the stage for Jin-Woo's desperate struggle for survival.", "solo_leveling.txt"),
+                    new Novel.Chapter("Chapter 2: The high-stakes survival at the Cartenon Temple reaches its climax as Sung Jin-Woo uses his sharp observation skills to decipher the final commandment while the remaining hunters are being slaughtered by the terrifying stone statues. After completing the trial, Jin-Woo is left behind and mortally wounded on the altar, facing certain death as the statues close in for the final blow. However, just as his life is about to end, a mysterious system notification appears before him, offering him the chance to become a Player. In his final moments of consciousness, Jin-Woo accepts the mysterious contract of the System, triggering a complete transformation that saves his life and marks the beginning of his journey from the world's weakest hunter to an entity with the unique ability to level up.", "solo_leveling.txt")
                 )
         ));
         list.add(new Novel(
@@ -86,7 +86,7 @@ public class NovelData {
                 "Kim Dokja's favorite web novel comes to life, and he is the only one who knows how the world will end.",
                 R.drawable.novel1,
                 Arrays.asList(
-                    new Novel.Chapter("Chapter 1: Three Ways to Survive in a Ruined World", "omniscient_reader.txt")
+                    new Novel.Chapter("Chapter 1: Kim Dokja, an ordinary office worker whose only hobby is reading an unpopular web novel titled Three Ways to Survive in a Ruined World, which finally reaches its conclusion after ten long years. As he finishes the final chapter on his subway commute home, the world he knows suddenly comes to a terrifying halt as the reality of the novel begins to merge with his own. A mysterious creature known as a Dokkaebi appears, announcing the start of a Main Scenario that forces passengers to participate in a brutal game of survival where they must kill a living being to proceed. Armed with the unique knowledge of the story's entire plot, Dokja realizes he is the only person who knows how the world will end, transforming from a mere reader into the sole protagonist of a new, apocalyptic reality.", "omniscient_reader.txt")
                 )
         ));
         list.add(new Novel(
@@ -95,7 +95,7 @@ public class NovelData {
                 "King Grey has unrivaled strength, wealth, and prestige in a world governed by martial ability.",
                 R.drawable.novel2,
                 Arrays.asList(
-                    new Novel.Chapter("Chapter 1: A King's End", "tbate.txt")
+                    new Novel.Chapter("Chapter 1: King Grey, a powerful and unparalleled ruler in a world governed by martial ability, who nonetheless lives a life of profound isolation and emotional emptiness despite his immense status. Upon his sudden and mysterious death, he is shocked to find himself reborn as a helpless infant named Arthur Leywin in a vibrant new world filled with magic and wonder. Retaining the vast knowledge and disciplined mind of his previous life, Arthur quickly realizes that this world operates on the laws of \"Mana\" rather than the \"Ki\" he once mastered. The chapter highlights his internal transition as he resolves to live this new life differently, seeking the warmth of family and genuine connections that he lacked as a king, all while beginning his secret training to harness the magical potential of his new body.", "tbate.txt")
                 )
         ));
         return list;
